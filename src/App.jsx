db63e47decafd7bca2cf0a7c1e2cdae7d92b22b1
Khushhal <khushhal@Khushhals-MacBook-Pro.local>
2024-03-18 19:36:11 +0500

    Project Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import { CORE_CONCEPTS } from "./data.js";
+import { useState } from "react";
+
+import { CORE_CONCEPTS, EXAMPLES } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function clickHandler(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
+
   return (
     <div>
       <Header />
@@ -11,19 +19,52 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((item, index) => (
+              <CoreConcept key={index} {...item} />
+            ))}
           </ul>
         </section>
 
-        {/* <section id="examples">
+        <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
+            <TabButton
+              isSelected={selectedTopic === "components"}
+              onClick={() => clickHandler("components")}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "jsx"}
+              onClick={() => clickHandler("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "props"}
+              onClick={() => clickHandler("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "state"}
+              onClick={() => clickHandler("state")}
+            >
+              State
+            </TabButton>
           </menu>
-        </section> */}
+          {!selectedTopic ? (
+            <p>Please select a topic.</p>
+          ) : (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )}
+        </section>
       </main>
     </div>
   );
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,7 +1,9 @@
-export default function TabButton(props) {
+export default function TabButton({ children, onClick, isSelected }) {
   return (
     <li>
-      <button>{props.children}</button>
+      <button className={isSelected ? "active" : undefined} onClick={onClick}>
+        {children}
+      </button>
     </li>
   );
 }
